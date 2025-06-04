dcd0aad5f757c55f6cf3764090cac1f5adcd9dc9
Holy Rapper <tungyaop@gmail.com>
2025-06-04 01:38:49 +0000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -21,6 +22,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
